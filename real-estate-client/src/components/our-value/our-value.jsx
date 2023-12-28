199d131291f273ec99cb3e7056c8912c5d037d0a
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2023-12-28 14:52:32 -0800

    added image to our value section

--- real-estate-client/src/components/our-value/our-value.jsx
+++ real-estate-client/src/components/our-value/our-value.jsx
@@ -4,12 +4,15 @@ import './value.css';
 const Value = () => {
   return (
     <div className='value-container'>
-      <div className="value-img">
-        Image here
-      </div>
-      <div className="desc">
-        <h4>Our Value</h4>
-        <h3>Value We Give to You</h3>
+      <img src="apartment-outside-img.jpg" alt="Fancy apartment building" className='value-image'/>
+      <div className="value-desc">
+        <div className="value-title flex-col-start">
+          <span className='primary-text'>Our Value</span>
+          <span className='orange-text'>What We Give to You</span>
+        </div>
+        <div className="drop-down-options">
+
+        </div>
       </div>
     </div>
   )
